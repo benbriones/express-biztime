c61f29a2ea70eb20a16796e059e0b93ce9cc697c
Jack Norquist <jack.norquist@outlook.com>
2024-03-21 17:27:03 -0500

    Add Invoice and Update Invoice Routes

--- routes/invoices.js
+++ routes/invoices.js
@@ -39,7 +39,7 @@ router.get('/:id', async function (req, res) {
 
   const invoice = results.rows[0];
 
-  if (!invoice) throw new NotFoundError(`Can't find invoice, ${id}`)
+  if (!invoice) throw new NotFoundError(`Can't find invoice, ${id}`);
 
 
   const company = await db.query(
@@ -54,4 +54,59 @@ router.get('/:id', async function (req, res) {
   return res.json({ invoice });
 });
 
+
+/**Recieves json.  Returns info on the new invoice, JSON  like
+ * {invoice: {id, comp_code, amt,...}}*/
+router.post('/', async function (req, res, next) {
+
+  if (!req.body) throw new BadRequestError("Must include body"); // dont pass anything
+
+  const { comp_code, amt } = req.body;
+
+  const hasUndefined = [comp_code, amt].some(input => input === undefined);
+  if (hasUndefined) throw new BadRequestError("Request must include all required fields");
+
+
+  const companyResults = await db.query(
+    `SELECT code
+    FROM companies
+    WHERE code = $1`,
+    [comp_code]
+
+  );
+  const companyCodeExists = companyResults.rows[0];
+
+  if (!companyCodeExists) throw new NotFoundError("Company code not found ");
+
+  const results = await db.query(
+    `INSERT INTO invoices (comp_code, amt)
+    VALUES ($1, $2)
+    RETURNING comp_code, amt`,
+    [comp_code, amt]
+  );
+
+  const company = results.rows[0];
+
+  return res.status(201).json({ company });
+
+});
+
+
+router.put('/:id', async function (req, res) {
+  const id = req.params.id;
+  const amt = req.body.amt;
+  const results = await db.query(
+    `UPDATE invoices
+    SET amt = $1
+    WHERE id = $2
+    RETURNING id, comp_code, amt, paid, add_date, paid_date`,
+    [amt, id]
+  );
+  const invoice = results.rows[0];
+  if (!invoice) throw new NotFoundError("Company code not found ");
+
+  return res.json({ invoice: invoice });
+
+
+});
 module.exports = router;
